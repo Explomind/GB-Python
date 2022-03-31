7996f6487e1bb4293f17fc7b03543bca3fd1e0b9
Explomind <explomind@gmail.com>
2022-03-31 13:45:28 +0300

    Task 2_5 modified, math and recur vars added

--- Task2_5.py
+++ Task2_5.py
@@ -23,6 +23,22 @@ def count_circle(amount, drop):
     return last
 
 
+def counting_rhyme(amount, drop):
+    count = 0
+    for i in range(1, amount + 1):
+        count = (count + drop) % i
+    return count + 1
+
+
+def counting_rhyme_rec(amount, drop):
+    if amount == 1:
+        return 1
+    else:
+        return (counting_rhyme_rec(amount - 1, drop) + drop - 1) % amount + 1
+
+
 amount_in_circle = int(input('Input amount of people in circle: '))
 drop_number = int(input('Input number of man who is to drop from circle: '))
-print('Last man: {0}'.format(count_circle(amount_in_circle, drop_number)))
+print('Last man (list): {0}'.format(count_circle(amount_in_circle, drop_number)))
+print('Last man (math): {0}'.format(counting_rhyme(amount_in_circle, drop_number)))
+print('Last man (recursion): {0}'.format(counting_rhyme_rec(amount_in_circle, drop_number)))
